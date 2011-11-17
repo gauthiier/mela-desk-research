480ca2791544559dff1a8932e202d9ecdd789e13
dviid <dviid@ciid.dk>
2011-11-17 23:04:40 +0100

    fixed map.js callbacks

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ function init() {
 function dataLoaded_cb(casesList) {
   console.log("Number of loaded cases: " + casesList.length);
   
-  var clusters = new Clusters(casesList);
-  //var map = new Map();
+  //var clusters = new Clusters(casesList);
+  var map = new Map();
   //map.displayCases(casesList);
   
 }
--- js/map.js
+++ js/map.js
@@ -1,15 +1,17 @@
 function Map() {
-    var latlng = new google.maps.LatLng(22.593, 1.757);
-    var myOptions = {
-      zoom: 2,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.gmap = new google.maps.Map(document.getElementById("mapcontent"), myOptions);
+  console.log('Loading map...');
+  var latlng = new google.maps.LatLng(22.593, 1.757);
+  var myOptions = {
+    zoom: 2,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  this.gmap = new google.maps.Map(document.getElementById("mapcontent"), myOptions);
 }
 
 var timeout = 700;
 Map.prototype.displayCases = function(casesList) {
+  console.log('Loading cases...');
   var who = this;
   for(var i=0; i<casesList.length; i++) {
         setTimeout((function close(k) {
@@ -21,34 +23,37 @@ Map.prototype.displayCases = function(casesList) {
 }
 
 Map.prototype.marker_cb = function (melacase) {
-    var geocoder = new google.maps.Geocoder();
-    var pos = geocoder.geocode({'address' : melacase.nameoftheinstitutionorganisation + ' , '
-                                          + melacase.city + ' , '
-                                          + melacase.country
-                               },
-                               function(res, status) {
+  console.log('marker_cb');
+  var geocoder = new google.maps.Geocoder();
+  var pos = geocoder.geocode({'address' : melacase.nameoftheinstitutionorganisation + ' , '
+                                        + melacase.city + ' , '
+                                        + melacase.country
+                             },
+                             function close(map) {
+                               return function(r, s) {
                                     if(status == google.maps.GeocoderStatus.OK && res.length > 0) {
-                                        marker(res[0].geometry.location, melacase);
+                                        map.marker(res[0].geometry.location, melacase);
                                     }
-                                }
-                              );
+                                }(this)
+                             });
 }
 
 Map.prototype.marker = function (location, melacase) {
-    var latlng = location;
-    var m = new google.maps.Marker({
-       position: latlng,
-       map: this.gmap,
-       title: melacase.nameoftheinstitutionorganisation,
-       draggable: false
-    });
-    google.maps.event.addListener(m, 'click',
-        function close(k) {
-          return function() {
-            showDetails(k);
-          }
-        }(melacase)
-    );
+  console.log('marker');
+  var latlng = location;
+  var m = new google.maps.Marker({
+     position: latlng,
+     map: this.gmap,
+     title: melacase.nameoftheinstitutionorganisation,
+     draggable: false
+  });
+  google.maps.event.addListener(m, 'click',
+      function close(k) {
+        return function() {
+          showDetails(k);
+        }
+      }(melacase)
+  );
 }
 
 
