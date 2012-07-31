523f765ae42870ab53c8b59f5263541ba8a33289
vorg <vorg@Marcins-MacBook-Pro-3.local>
2012-07-31 11:57:40 +0200

    More visualizations

--- viz/cases-book/js/common.js
+++ viz/cases-book/js/common.js
@@ -1,26 +1,230 @@
-function groupCasesBy(data, fieldId, fieldName, mappingFunc) {
-  var clusters = {};
+var Fields = {
+  FieldOfActivity : 8, //c
+  Year : 10, // t
+  Description : 22, //ta
+  FocusOfTechnology : 23, //r
+  TypeOfMediaExhibited : 26, // c
+  ActionsAndMechanicsOfInteraction : 27, //ta
+  WhyIsItOfInterestForMeLAsResearchField05 : 29, //ta
+  AudienceEngagement : 34, //rng
+  ExhibitionElements : 36, //rng
+  TechnologyMetaphor : 37 //rng
+}
 
-  data.forEach(function(melaCase) {
-    var value = melaCase[fieldId];
-    if (mappingFunc)
-      value = mappingFunc(value);
-    if (!clusters[value]) {
-      clusters[value] = [];
+var Colors = {
+  White : chroma.hex('#fff'),
+  SkyBlue : chroma.hex('#55c1dc'),
+  TomatoRed : chroma.hex('#eb6909'),
+  Cyan : chroma.hex('#00EDBA')
+}
+
+//-----------------------------------------------------------------------------
+
+Utils = {};
+Utils.mean = function() {
+
+}
+
+//-----------------------------------------------------------------------------
+
+function DataSource() {
+  this.data = [];
+  this.isoCountries = [];
+  this.loadCountryList();
+}
+
+DataSource.prototype.loadCountryList = function() {
+  var self = this;
+  d3.text("data/iso-countries.csv", function(list) {
+    self.isoCountries = CSVToArray(list);
+
+    if (self.onLoadCallback) {
+      self.onLoadCallback();
+      self.onLoadCallback = null;
     }
-    clusters[value].push(melaCase)
   });
-  var result = [];
-  for(var value in clusters) {
-    var list = {  cases:clusters[value] };
-    list[fieldName] = value;
-    result.push(list);
-  }
-  return result;
 }
 
-function parseYear(yearStr) {
-  var yearNumber = Number(yearStr.substr(0, 4));
-  if (isNaN(yearNumber)) return "  NA";
-  return yearNumber;
+
+DataSource.prototype.load = function(file, callback) {
+  var self = this;
+  d3.text(file, function(dataStr) {
+    var data = CSVToArray(dataStr);
+
+    var info = [];
+    info.push("Field05 : " + data.length + " cases");
+    var verbose = false;
+
+    if (verbose) console.log(info);
+    //build list of all fields and their types
+    var fieldNames = data[0];
+    var fieldTypes = data[2];
+    var fieldValues = data[3];
+    var fields = [];
+    for(var i in fieldNames) {
+      if (fieldTypes[i] != '') {
+        fields[i] = {
+          name : fieldNames[i],
+          type : fieldTypes[i],
+          values : fieldValues[i].split("|")
+        }
+        if (verbose) console.log(i, fieldNames[i], fieldTypes[i])
+      }
+      else {
+        fields[i] = null;
+      }
+    }
+    data.splice(0, 4); //remove metadata
+
+    self.data = data;
+    self.fields = fields;
+
+    if (self.isoCountries.length > 0) callback();
+    else self.onLoadCallback = callback; //wait for iso countries to finish loading
+  });
 }
+
+DataSource.prototype.countryNameToIso = function(name) {
+  var nameUpper = name.toUpperCase();
+  if (name == "United States of America") {
+    nameUpper = "UNITED STATES";
+  }
+  var matches = _.filter(this.isoCountries, function(country) {
+    return nameUpper == country[0];
+  })
+  if (matches.length > 0) {
+    return matches[0][1];
+  }
+  else {
+    console.log("No ISO found for " + name);
+    return "Unknown";
+  }
+}
+
+DataSource.prototype.findAndCountAllPossibleValues = function(data, fieldId) {
+  function getValues(fieldId) {
+    return function(melaCase) {
+      return melaCase[fieldId];
+    }
+  }
+
+  function sorterDesc(property) {
+    return function(a, b) {
+      return -(a[property] - b[property]);
+    }
+  }
+
+  var casesValues = data.map(getValues(fieldId));
+  var values = {};
+
+  function addValue(value) {
+    if (!values[value]) values[value] = 0;
+    values[value]++;
+  }
+
+  casesValues.forEach(function(value) {
+    if (value.indexOf("|") !== -1)
+      value.split("|").forEach(addValue);
+    else
+      addValue(value);
+  })
+
+  var result = [];
+  for(var value in values) {
+    result.push({value : value, count : values[value]});
+  }
+
+  return result.sort(sorterDesc("count"));
+}
+
+DataSource.prototype.findAllPossibleValues = function(data, fieldId) {
+  return this.findAndCountAllPossibleValues(data, fieldId).map(function(o) { return o.value; });
+}
+
+
+DataSource.prototype.unrollData = function(data, fieldId) {
+  function clone(melaCase) {
+    var newCase = {};
+    for(var i in melaCase) {
+      newCase[i] = melaCase[i];
+    }
+    return newCase;
+  }
+
+  var cases = [];
+  data.forEach(function(melaCase) {
+    var values = melaCase[fieldId].split("|");
+    for(var value in values) {
+      var newCase = clone(melaCase);
+      newCase[fieldId] = values[value];
+      cases.push(newCase);
+    }
+  })
+
+  return cases;
+}
+
+DataSource.prototype.sum = function(values) {
+  return values.reduce(function(total, value) { return total + value; }, 0);
+}
+
+DataSource.prototype.calcMean = function(values) {
+  return this.sum(values) / values.length;
+}
+
+DataSource.prototype.calcStdDev = function(values) {
+  var n = values.length;
+
+  var meanOfSquares = 0;
+  values.forEach(function(value) { meanOfSquares += value * value; })
+  meanOfSquares = meanOfSquares / n;
+
+  var mean = this.calcMean(values);
+  var squareOfMean = mean * mean;
+
+  return Math.sqrt(meanOfSquares - squareOfMean);
+}
+
+DataSource.prototype.calcStdDev2 = function(values) {
+  var n = values.length;
+  var mean = this.calcMean(values);
+
+  var dsum = 0;
+  for(var i=0; i<n; i++) {
+    dsum += (values[i] - mean) * (values[i] - mean);
+  }
+
+  return Math.sqrt(dsum/n);
+
+}
+
+
+DataSource.prototype.valuesToNumbers = function(data, fieldId) {
+  var possibleValues = this.findAllPossibleValues(data, fieldId);
+  return data.map(function(melaCase) {
+    return possibleValues.indexOf(melaCase[fieldId]);
+  })
+}
+
+DataSource.prototype.correlation = function(data, fieldId1, fieldId2) {
+  var numValues1 = this.valuesToNumbers(data, fieldId1);
+  var numValues2 = this.valuesToNumbers(data, fieldId2)
+  var mean1 = this.calcMean(numValues1);
+  var mean2 = this.calcMean(numValues2);
+  var stdDev1 = this.calcStdDev(numValues1);
+  var stdDev2 = this.calcStdDev(numValues2);
+
+  var n = data.length;
+  var sum = 0;
+  for(var i=0; i<n; i++) {
+    sum += numValues1[i] * numValues2[i];
+  }
+
+  return (1/(n - 1) * (sum - n * mean1 * mean2)) / (stdDev1 * stdDev2);
+}
+
+/*
+
+*/
+
+
--- viz/cases-book/js/options.js
+++ viz/cases-book/js/options.js
@@ -1,6 +1,6 @@
 function Options() {
   this.items = [];
-  
+
   this.parseUrlString();
 }
 
@@ -10,18 +10,18 @@ Options.prototype.parseUrlString = function() {
     var queryStr = document.location.href.substr(queryStart + 1);
     var params = queryStr.split("&").map(function(paramStr) {
       var param = paramStr.split("=");
-      return { 
-        name : param[0], 
-        value : (param[1] == "true") ? true : false 
+      return {
+        name : param[0],
+        value : (param[1] == "true") ? true : false
       }
     })
     if (params.length > 0) {
         this.items = params;
-    }    
+    }
   }
 }
 
-Options.prototype.add = function(name, labelStr, value) {
+Options.prototype.add = function(name, labelStr, value, group) {
   var item = this.getItemByName(name);
   if (!item) {
     item =  {
@@ -30,25 +30,40 @@ Options.prototype.add = function(name, labelStr, value) {
     }
     this.items.push(item);
   }
-  
+
+  item.group = group;
+
   var label = document.createElement("label");
   var labelText = document.createTextNode(labelStr);
   var checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   label.appendChild(checkbox);
-  label.appendChild(labelText);  
+  label.appendChild(labelText);
   document.getElementById("options").appendChild(label);
-  
+
   if (item.value) checkbox.checked = item.value;
-  
+
   var self = this;
-  
+
   checkbox.addEventListener('click', function() {
     item.value = checkbox.checked;
+
+    console.log(item.group, item.value);
+    if (item.group && item.value) {
+      self.items.forEach(function(otherItem) {
+        if (otherItem != item && otherItem.group == item.group) {
+          otherItem.value = false;
+        }
+      })
+    }
+
     self.refreshPage();
   })
-  
-  
+}
+
+Options.prototype.addBreak = function() {
+  var breakEl = document.createElement("hr");
+  document.getElementById("options").appendChild(breakEl);
 }
 
 Options.prototype.getItemByName = function(name) {
@@ -59,9 +74,13 @@ Options.prototype.getValue = function(name) {
   return this.getItemByName(name).value;
 }
 
+Options.prototype.setValue = function(name, value) {
+  return this.getItemByName(name).value = value;
+}
+
 Options.prototype.refreshPage = function() {
   function getNameValue(o) { return o.name + "=" + o.value; }
-  
+
   var queryString = this.items.map(getNameValue).join("&");
   document.location.href = document.location.pathname + "?" + queryString;
 }
